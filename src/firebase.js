97f58d5e49eeea366762b7e95574cf5ddac3e8e6
Cristian Ramirez <112449760+sirc251@users.noreply.github.com>
2024-08-29 15:31:54 -0500

    Update Firebase
    Se carga el codigo , que por algun error anteriormente no cargo .

---
+++ src/firebase.js
@@ -0,0 +1,34 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// Importa otras funciones si las necesitas, por ejemplo, Firestore
+import { getFirestore } from "firebase/firestore"; 
+import { getAuth } from "firebase/auth";
+
+// Configuración de tu aplicación web Firebase
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBHNN_5X4WnOSOK7my8uRfvtAAEexTLwrE",
+  authDomain: "movileshibrida.firebaseapp.com",
+  projectId: "movileshibrida",
+  storageBucket: "movileshibrida.appspot.com",
+  messagingSenderId: "28336147441",
+  appId: "1:28336147441:web:e6b5b41a301d1e365e9250",
+  measurementId: "G-8KGQF9Q8E6"
+};
+
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inicializa Firebase Analytics (opcional)
+const analytics = getAnalytics(app);
+
+// Inicializa Firestore (si lo necesitas)
+const db = getFirestore(app);
+
+// Inicializa Firebase Authentication
+const auth = getAuth(app);
+
+export { app, analytics, db,auth };
+
+// Codigo de firebase
